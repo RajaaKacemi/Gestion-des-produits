a6e35860ca0002c9c832e4887688a16cfbb30815
RajaaKacemi <rajaa01.kacemi@gmail.com>
2025-06-09 18:33:02 +0100

    Add product

--- src/main/java/com/example/bdccensetspringenset/BdccEnsetSpringApplication.java
+++ src/main/java/com/example/bdccensetspringenset/BdccEnsetSpringApplication.java
@@ -5,15 +5,15 @@ import com.example.bdccensetspringenset.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class BdccEnsetSpringEnsetApplication {
+//@SpringBootApplication
+public class BdccEnsetSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BdccEnsetSpringEnsetApplication.class, args);
+        SpringApplication.run(BdccEnsetSpringApplication.class, args);
     }
 
     @Bean
---
+++ src/main/java/com/example/bdccensetspringenset/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.bdccensetspringenset.security;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(BeanFactory beanFactory){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("user3").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build()
+
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(Customizer.withDefaults())
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/", "/index", "/login", "/newProduct", "/saveProduct", "/delete", "/h2-console/**").permitAll()
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(eh -> eh.accessDeniedPage("/notAuthorized"))
+                .build();
+    }
+
+
+}
